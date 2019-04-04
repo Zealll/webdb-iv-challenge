b4d61648fffa2979f043b036841fb8378eccda6c
Zealll <eriznis@gmail.com>
2019-04-04 18:52:28 -0400

    migrations completed

---
+++ data/migrations/20190404175722_cooking_table.js
@@ -0,0 +1,56 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('dishes', tbl => {
+    tbl.increments();
+    tbl.string('name', 100).notNullable().unique()
+  })
+
+  .createTable('recipes', tbl => {
+      tbl.increments()
+      tbl.string('instructions', 200).notNullable()
+      tbl.integer('dish_id')
+         .unsigned()
+         .notNullable()
+         .references('id')
+         .inTable('dishes')
+         .onDelete('CASCADE')
+         .onUpdate('CASCADE')
+  })
+
+  .createTable('ingredients', tbl => {
+      tbl.increments()
+      tbl.string('name', 100).notNullable().unique()
+  })
+
+  .createTable('recipes_ingredients', tbl => {
+      tbl.increments()
+      
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      tbl
+        .integer('ingredient_id')
+        .unsigned('notNullable')
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      tbl.float('quantity', precision, scale)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('recipes_ingredients')
+    .dropTableIfExists('ingerdients')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('dishes')
+};
